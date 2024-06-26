3a8152bc4a3e3a31bab7405a2dcd4a72ab4a020d
lkoida <leonidkoida@gmail.com>
2024-06-26 10:15:36 +0300

    renamed test cases in find.test for easier mavigation and test run

--- tests/find.test.js
+++ tests/find.test.js
@@ -1,26 +1,28 @@
-import {describe, it} from "node:test";
-import assert from "node:assert"
+import { describe, it } from "node:test";
+import assert from "node:assert";
 import { findActive, findByUserFriendName } from "../exercises/find.js";
-import {users} from "../data/source-data.js";
+import { users } from "../data/source-data.js";
 
+describe("find", () => {
+  it("findActive", () => {
+    const activeUser = findActive(users);
+    assert.strictEqual(activeUser._id, "66091325c9ca3a42798248ae");
+  });
 
-describe('should run correctly all scripts in find.js', () => {
-    it('should find user with the name john', () => {
-        const activeUser = findActive(users);
-        assert.strictEqual(activeUser._id, '66091325c9ca3a42798248ae')
-    });
+  it("findByUserFriendName", () => {
+    const expectedFriendName = "Lidia";
+    const user = findByUserFriendName(users, expectedFriendName);
 
-    it('should find user whose friend named Lidia', () => {
-        const expectedFriendName = 'Lidia'
-        const user = findByUserFriendName(users, expectedFriendName)
+    assert.strictEqual(
+      user.friends.some((friend) => friend.firstName === expectedFriendName),
+      true
+    );
+  });
 
-        assert.strictEqual(user.friends.some(friend => friend.firstName ===  expectedFriendName), true);
-    });
+  it("should return null if no friend found", () => {
+    const expectedFriendName = "Pablo";
+    const user = findByUserFriendName(users, expectedFriendName);
 
-    it('should return null if no friend found', () => {
-        const expectedFriendName = 'Pablo'
-        const user = findByUserFriendName(users, expectedFriendName)
-
-        assert.strictEqual(user, undefined);
-    });
+    assert.strictEqual(user, undefined);
+  });
 });
