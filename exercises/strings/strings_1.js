04bfa0cef98fa5dd3d987e2f731ac17ad3bcffab
lkoida <leonidkoida@gmail.com>
2024-08-08 17:20:40 +0300

    added first exercises for string manipulation

---
+++ exercises/strings/strings_1.js
@@ -0,0 +1,36 @@
+/**
+ * Create acronym from provided string
+ * Example if the string 'hello world' is provided
+ * then the 'HW' should be returned
+ *
+ * Use the split,reduce,charAt and toUpperCase methods
+ * @param {string} input
+ *
+ * @returns {string}
+ */
+export const createAcronym_1 = (input) => {
+}
+
+/**
+ * The same problem but another methods to use
+ *
+ * Methods: split, map,charAt,join, toUpperCase
+ *
+ * @param {string} input
+ *
+ * @returns {string}
+ */
+export const createAcronym_2 = (input) => {
+}
+
+/**
+ * Check if provided string is palindrome
+ * Return true or false depending on result
+ * (let's not take in consideration any special characters like comas and points)
+ *
+ * @param {string} input
+ *
+ * @returns {boolean}
+ */
+export const checkOnPalindrome = (input) => {
+}
---
+++ tests/strings/strings_1.test.js
@@ -0,0 +1,16 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+import {createAcronym_1, createAcronym_2, checkOnPalindrome} from "../../exercises/strings/strings_1.js";
+
+describe('strings_1', () => {
+    it('should render the acronym form provided string', () => {
+        assert.strictEqual(createAcronym_1('some funny string'), 'SFS')
+    });
+    it('should render the acronym form provided string with different implementation', () => {
+        assert.strictEqual(createAcronym_2('some funny string'), 'SFS')
+    });
+
+    it('should return true if string is palindrome', () => {
+        assert.equal(checkOnPalindrome('А роза упала на лапу азора'), true)
+    });
+});
