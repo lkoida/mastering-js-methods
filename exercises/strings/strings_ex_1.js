a7b9cbe4c4f4a522af56ebc39c4510088902acb2
lkoida <leonid.koida@alpheya.com>
2024-08-19 15:23:30 +0300

    added more simple exercises to train before the combine them

--- exercises/strings/strings_ex_1.js
+++ exercises/strings/strings_ex_1.js
@@ -1,4 +1,31 @@
 /**
+ *Convert string to upper case
+ *
+ * @param {string} input
+ * @returns {string}
+ */
+export const convertStringToUpperCase = (input) => {
+  return input.toUpperCase();
+};
+
+/**
+ *Convert string to lower case
+ * @param {string} input
+ * @return {string}
+ */
+export const convertStringToLowerCase = (input) => {
+  return input.toLowerCase();
+};
+
+/**
+ * @param {string} inputString
+ * @param {number} position
+ */
+export const getletterFromPosition = (inputString, position) => {
+  return inputString.charAt(position);
+};
+
+/**
  * Create acronym from provided string
  * Example if the string 'hello world' is provided
  * then the 'HW' should be returned
@@ -8,8 +35,7 @@
  *
  * @returns {string}
  */
-export const createAcronym_1 = (input) => {
-}
+export const createAcronym_1 = (input) => {};
 
 /**
  * The same problem but another methods to use
@@ -20,8 +46,7 @@ export const createAcronym_1 = (input) => {
  *
  * @returns {string}
  */
-export const createAcronym_2 = (input) => {
-}
+export const createAcronym_2 = (input) => {};
 
 /**
  * Check if provided string is palindrome
@@ -32,5 +57,4 @@ export const createAcronym_2 = (input) => {
  *
  * @returns {boolean}
  */
-export const checkOnPalindrome = (input) => {
-}
+export const checkOnPalindrome = (input) => {};
--- tests/strings/strings_ex_1.test.js
+++ tests/strings/strings_ex_1.test.js
@@ -1,16 +1,45 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import {createAcronym_1, createAcronym_2, checkOnPalindrome} from "../../exercises/strings/strings_ex_1.js";
+import {
+  createAcronym_1,
+  createAcronym_2,
+  checkOnPalindrome,
+  convertStringToUpperCase,
+  convertStringToLowerCase,
+  getletterFromPosition,
+} from "../../exercises/strings/strings_ex_1.js";
 
-describe('strings_1', () => {
-    it('should render the acronym form provided string', () => {
-        assert.strictEqual(createAcronym_1('some funny string'), 'SFS')
-    });
-    it('should render the acronym form provided string with different implementation', () => {
-        assert.strictEqual(createAcronym_2('some funny string'), 'SFS')
-    });
+describe("strings_1", () => {
+  it("should return uppercased string", () => {
+    assert.strictEqual(convertStringToUpperCase("Hello World"), "HELLO WORLD");
+  });
 
-    it('should return true if string is palindrome', () => {
-        assert.equal(checkOnPalindrome('А роза упала на лапу азора'), true)
-    });
+  it("should return lowercased string", () => {
+    assert.strictEqual(convertStringToLowerCase("Hello World"), "hello world");
+  });
+
+  it("should return character form the position", () => {
+    const assertions = [
+      { string: "hello world", charAt: 1, expected: "e" },
+      { string: "hello world", charAt: 2, expected: "l" },
+      { string: "hello world", charAt: 0, expected: "h" },
+      { string: "hello world", charAt: 5, expected: " " },
+    ];
+
+    for (const { string, charAt, expected } of assertions) {
+      assert.strictEqual(getletterFromPosition(string, charAt), expected);
+    }
+  });
+
+
+  //   it("should render the acronym form provided string", () => {
+  //     assert.strictEqual(createAcronym_1("some funny string"), "SFS");
+  //   });
+  //   it("should render the acronym form provided string with different implementation", () => {
+  //     assert.strictEqual(createAcronym_2("some funny string"), "SFS");
+  //   });
+
+  //   it("should return true if string is palindrome", () => {
+  //     assert.equal(checkOnPalindrome("А роза упала на лапу азора"), true);
+  //   });
 });
