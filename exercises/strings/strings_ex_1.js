60f47ef19415c2a47b0a0002801ba8a706ef83da
lkoida <leonidkoida@gmail.com>
2024-08-29 18:37:53 +0300

    added more exercises on strings and updated tests

--- exercises/strings/strings_ex_1.js
+++ exercises/strings/strings_ex_1.js
@@ -1,4 +1,23 @@
 /**
+ * Return the array form the string
+ * @param {string} str
+ *
+ * @returns {Array<string>}
+ */
+export const stringToArrayOfWords = (str) => {
+};
+
+/**
+ *
+ * @param {Array<string>} input
+ * @param {string} delimiter
+ *
+ * return {string}
+ */
+export const arrayOfWordsToString = (input, delimiter) => {
+}
+
+/**
  *Convert string to upper case
  *
  * @param {string} input
@@ -20,7 +39,6 @@ export const convertStringToLowerCase = (input) => {
  * @param {number} position
  */
 export const getLetterFromPosition = (inputString, position) => {
-  return inputString.charAt(position);
 };
 
 /**
@@ -33,18 +51,22 @@ export const getLetterFromPosition = (inputString, position) => {
  *
  * @returns {string}
  */
-export const createAcronym_1 = (input) => {};
+export const createAcronym_1 = (input) => {
+
+};
 
 /**
  * The same problem but another methods to use
  *
- * Methods: split, map,charAt,join, toUpperCase
+ * Methods: split, map,charAt ,join, toUpperCase
  *
  * @param {string} input
  *
  * @returns {string}
  */
-export const createAcronym_2 = (input) => {};
+export const createAcronym_2 = (input) => {
+
+};
 
 /**
  * Check if provided string is palindrome
@@ -55,4 +77,14 @@ export const createAcronym_2 = (input) => {};
  *
  * @returns {boolean}
  */
-export const checkOnPalindrome = (input) => {};
+export const checkOnPalindrome = (input) => {
+    // we use regular expression to remove all special characters from the string
+    // In general, what this is means:
+    // in `replace()` method we have slashes which tells us that we are using regular expression
+    // inside the slashes we have square brackets which works as a range we want to find
+    // inside the square brackets we have `^` which is a negation,
+    // and then we have `a-zA-Z0-9` which is a range of characters we want to find,
+    // and then we have `/g` which is a global flag,
+    // so, in the end, we are saying that we want to find all characters that are not in the range of a-z, A-Z, and 0-9
+    const stringWithoutSpecialCharacters = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+};
--- tests/strings/strings_ex_1.test.js
+++ tests/strings/strings_ex_1.test.js
@@ -1,45 +1,110 @@
-import { describe, it } from "node:test";
+import {describe, it} from "node:test";
 import assert from "node:assert";
 import {
-  createAcronym_1,
-  createAcronym_2,
-  checkOnPalindrome,
-  convertStringToUpperCase,
-  convertStringToLowerCase,
-  getLetterFromPosition,
+    createAcronym_1,
+    createAcronym_2,
+    checkOnPalindrome,
+    convertStringToUpperCase,
+    convertStringToLowerCase,
+    getLetterFromPosition,
+    stringToArrayOfWords,
+    arrayOfWordsToString,
 } from "../../exercises/strings/strings_ex_1.js";
 
 describe("strings_1", () => {
-  it("should return uppercased string", () => {
-    assert.strictEqual(convertStringToUpperCase("Hello World"), "HELLO WORLD");
-  });
 
-  it("should return lowercased string", () => {
-    assert.strictEqual(convertStringToLowerCase("Hello World"), "hello world");
-  });
+    it("should convert a string to an array of words", () => {
+        const testCases = [
+            {input: "hello world", expected: ["hello", "world"]},
+            {input: "a b c", expected: ["a", "b", "c"]},
+            {input: "one two three", expected: ["one", "two", "three"]},
+            {input: "singleword", expected: ["singleword"]},
+            {input: "", expected: [""]},
+        ];
 
-  it("should return character form the position", () => {
-    const assertions = [
-      { string: "hello world", charAt: 1, expected: "e" },
-      { string: "hello world", charAt: 2, expected: "l" },
-      { string: "hello world", charAt: 0, expected: "h" },
-      { string: "hello world", charAt: 5, expected: " " },
-    ];
+        for (const {input, expected} of testCases) {
+            assert.deepStrictEqual(stringToArrayOfWords(input), expected);
+        }
+    });
 
-    for (const { string, charAt, expected } of assertions) {
-      assert.strictEqual(getLetterFromPosition(string, charAt), expected);
-    }
-  });
+    it("should join an array of words into a string with the given delimiter", () => {
+        const testCases = [
+            {input: ["hello", "world"], delimiter: " ", expected: "hello world"},
+            {input: ["a", "b", "c"], delimiter: "-", expected: "a-b-c"},
+            {input: ["one", "two", "three"], delimiter: ",", expected: "one,two,three"},
+            {input: ["singleword"], delimiter: " ", expected: "singleword"},
+            {input: [], delimiter: " ", expected: ""},
+        ];
+
+        for (const {input, delimiter, expected} of testCases) {
+            assert.strictEqual(arrayOfWordsToString(input, delimiter), expected);
+        }
+    });
+
+    it("should return uppercased string", () => {
+        assert.strictEqual(convertStringToUpperCase("Hello World"), "HELLO WORLD");
+    });
+
+    it("should return lowercased string", () => {
+        assert.strictEqual(convertStringToLowerCase("Hello World"), "hello world");
+    });
+
+    it("should return character form the position", () => {
+        const assertions = [
+            {string: "hello world", charAt: 1, expected: "e"},
+            {string: "hello world", charAt: 2, expected: "l"},
+            {string: "hello world", charAt: 0, expected: "h"},
+            {string: "hello world", charAt: 5, expected: " "},
+        ];
+
+        for (const {string, charAt, expected} of assertions) {
+            assert.strictEqual(getLetterFromPosition(string, charAt), expected);
+        }
+    });
 
 
-  //   it("should render the acronym form provided string", () => {
-  //     assert.strictEqual(createAcronym_1("some funny string"), "SFS");
-  //   });
-  //   it("should render the acronym form provided string with different implementation", () => {
-  //     assert.strictEqual(createAcronym_2("some funny string"), "SFS");
-  //   });
+    it("should create an acronym from a provided string", () => {
+        const testCases = [
+            {input: "hello world", expected: "HW"},
+            {input: "some funny string", expected: "SFS"},
+            {input: "JavaScript Object Notation", expected: "JON"},
+            {input: "HyperText Markup Language", expected: "HML"},
+            {input: "singleword", expected: "S"},
+            {input: "", expected: ""},
+        ];
 
-  //   it("should return true if string is palindrome", () => {
-  //     assert.equal(checkOnPalindrome("А роза упала на лапу азора"), true);
-  //   });
+        for (const {input, expected} of testCases) {
+            assert.strictEqual(createAcronym_1(input), expected);
+        }
+    });
+    it("should create an acronym from a provided string using split, map, charAt, join, and toUpperCase methods", () => {
+        const testCases = [
+            {input: "hello world", expected: "HW"},
+            {input: "some funny string", expected: "SFS"},
+            {input: "JavaScript Object Notation", expected: "JON"},
+            {input: "HyperText Markup Language", expected: "HML"},
+            {input: "singleword", expected: "S"},
+            {input: "", expected: ""},
+        ];
+
+        for (const {input, expected} of testCases) {
+            assert.strictEqual(createAcronym_2(input), expected);
+        }
+    });
+
+    it("should return true for palindromes and false for non-palindromes", () => {
+        const testCases = [
+            { input: "A man a plan a canal Panama", expected: true },
+            { input: "racecar", expected: true },
+            { input: "hello", expected: false },
+            { input: "No lemon, no melon", expected: true },
+            { input: "Was it a car or a cat I saw", expected: true },
+            { input: "Not a palindrome", expected: false },
+            { input: "А роза упала на лапу азора", expected: true },
+        ];
+
+        for (const { input, expected } of testCases) {
+            assert.strictEqual(checkOnPalindrome(input), expected);
+        }
+    });
 });
